a35cc603240adabbe0419970f25494fdf1e80070
abhidp <abhi358@gmail.com>
2020-08-27 21:52:14 +1000

    add color to setup script

--- util/setupRepo.sh
+++ util/setupRepo.sh
@@ -1,9 +1,9 @@
 arr=("$PWD/certificates/PROD" "$PWD/certificates/SANDBOX" "$PWD/certificates/UAT" "$PWD/certificates/TEAMS")
 for d in "${arr[@]}"; do
     if [ -d "$d" ]; then
-        echo "Already present : $d"
+        echo "\033[33mFolder exists : $d \033[0m"
     else
-        echo "Creating folder : $d"
         mkdir "$d"
+        echo "\033[32mFolder created : $d \033[0m"
     fi
 done
